f4e78cabb8df479080f936ffae736cd402acd2ba
Nitz <nitz.raz@gmail.com>
2016-01-25 13:46:38 +0200

    Fixed wrong condition in pip installer

--- resources/pip_installer.rb
+++ resources/pip_installer.rb
@@ -7,7 +7,7 @@ action :install do
   end
 
   execute 'install-pip' do
-    not_if "#{python_path} -m pip &>/dev/null"
+    only_if "#{python_path} -m pip &>/dev/null"
     command "#{python_path} #{get_pip}"
   end
 end
