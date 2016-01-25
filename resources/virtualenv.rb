4f0dc3e3cfaa229d839f8e70748dd5ba88e15275
Nitz <nitz.raz@gmail.com>
2016-01-25 13:04:41 +0200

    Fixed interpreter typo

--- resources/virtualenv.rb
+++ resources/virtualenv.rb
@@ -1,5 +1,5 @@
 property :path, String, name_attribute: true
-property :intepreter, String
+property :interpreter, String
 property :owner, String, regex: Chef::Config[:user_valid_regex]
 property :group, String, regex: Chef::Config[:group_valid_regex]
 property :options, String # Additional options for venv initialization
@@ -14,7 +14,7 @@ action :create do
     user new_resource.owner if new_resource.owner
     group new_resource.group if new_resource.group
   end
-  execute "virtualenv #{"--python="+new_resource.intepreter if new_resource.intepreter} #{new_resource.options} #{new_resource.path}" do
+  execute "virtualenv #{"--python="+new_resource.interpreter if new_resource.interpreter} #{new_resource.options} #{new_resource.path}" do
     user new_resource.owner if new_resource.owner
     group new_resource.group if new_resource.group
     environment ({ 'HOME' => ::Dir.home(new_resource.owner) }) if new_resource.owner
