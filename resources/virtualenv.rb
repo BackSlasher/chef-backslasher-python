1ec2a457462cf81a4950b036259a18cc4adcd8c4
Nitz <nitz.raz@gmail.com>
2016-03-14 13:55:38 +0200

    Missed some property -> attribute

--- resources/virtualenv.rb
+++ resources/virtualenv.rb
@@ -1,5 +1,5 @@
-property :path, kind_of: String, name_attribute: true
-property :interpreter, kind_of: String
+attribute :path, kind_of: String, name_attribute: true
+attribute :interpreter, kind_of: String
 attribute :owner, kind_of: String, regex: Chef::Config[:user_valid_regex]
 attribute :group, kind_of: String, regex: Chef::Config[:group_valid_regex]
 attribute :options, kind_of: String # Additional options for venv initialization
