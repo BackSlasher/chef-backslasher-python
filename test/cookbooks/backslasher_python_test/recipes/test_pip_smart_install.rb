db40f1d033e9eba53ed57af933d7e4cad7081d88
Nitz <nitz.raz@gmail.com>
2016-03-16 12:58:00 +0200

    Added more tests to smart_install

--- test/cookbooks/backslasher_python_test/recipes/test_pip_smart_install.rb
+++ test/cookbooks/backslasher_python_test/recipes/test_pip_smart_install.rb
@@ -9,9 +9,17 @@ backslasher_python_pip 'should_dsl smart_install first' do
   package_name 'should_dsl'
   version '2.1.1'
   virtualenv "/tmp/test_pip_smart_install"
+  smart_install true
 end
 
-ruby_block 'fail test_pip_smart_install' do
+ruby_block 'fail test_pip_smart_install first' do
+  block do
+    raise 'Smart install didnt install first instance'
+  end
+  not_if '/tmp/test_pip_smart_install/bin/pip list | grep should-dsl'
+end
+
+ruby_block 'fail test_pip_smart_install second' do
   action :nothing
   block do
     raise 'Smart install didnt stop second instance'
@@ -23,5 +31,5 @@ backslasher_python_pip 'should_dsl smart_install second' do
   package_name 'should-dsl<=2.1.1'
   virtualenv "/tmp/test_pip_smart_install"
   smart_install true
-  notifies :run, 'ruby_block[fail test_pip_smart_install]', :immediate
+  notifies :run, 'ruby_block[fail test_pip_smart_install second]', :immediate
 end
