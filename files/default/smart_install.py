4aa76e0d5b3a80208b19331cd6b054d63ae176c9
OmriManor <omri.manor@dynamicyield.com>
2016-03-22 11:35:39 +0200

    patching build wheel to do nothing

--- files/default/smart_install.py
+++ files/default/smart_install.py
@@ -1,13 +1,18 @@
-from pip.req import RequirementSet
-
+# Patch RequirementSet.install to collect to our array
 to_install=[]
-
+from pip.req import RequirementSet
 def my_install(self, install_options, global_options=(), *args, **kwargs):
     global to_install
     to_install.extend([r for r in self.requirements.values() if not r.satisfied_by])
-
 RequirementSet.install = my_install
 
+# Patch WheelBuilder.build to do nothing
+from pip.wheel import  WheelBuilder
+def my_build(self, autobuilding):
+    pass
+WheelBuilder.build = my_build
+
+# Emulate `pip install`
 import pip
 import sys
 args = sys.argv[1:]
